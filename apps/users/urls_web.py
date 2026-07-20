f2dd32516209c2347b9eb00e66ff4fa6570eeeab
MinatoMasamba <edmasamba100@gmail.com>
2026-07-20 08:52:43 +0200

    feat: implement backend view and URL for client dashboard

--- apps/users/urls_web.py
+++ apps/users/urls_web.py
@@ -1,10 +1,12 @@
 from django.urls import path
 
 from . import views_web
+from .views_web import client_dashboard_view  # Importer la nouvelle vue
 
 app_name = "chauffeur"
 
 urlpatterns = [
+    path("client/dashboard/", client_dashboard_view, name="client_dashboard"), # Ajout
     path("connexion/", views_web.ChauffeurLoginView.as_view(), name="chauffeur_login"),
     path("inscription/", views_web.ChauffeurRegistrationView.as_view(), name="chauffeur_registration"),
     path("app/", views_web.ChauffeurAppView.as_view(), name="chauffeur_app"),
